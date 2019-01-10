38b1ded170f87637475a0ef124d0d0a90584b548
Dennis Weiss <weiss.dennis98@yahoo.de>
2019-01-10 17:01:18 +0100

    Setting up register window now

--- client/register.py
+++ client/register.py
@@ -1,4 +1,22 @@
+from PyQt5.QtWidgets import QLineEdit, QVBoxLayout
 
 
 def setup_register_window(window):
-    print('Setting up window')
+    layout = QVBoxLayout()
+
+    register_username_field = QLineEdit()
+    register_username_field.setPlaceholderText('Username')
+
+    register_password_field = QLineEdit()
+    register_password_field.setPlaceholderText('Password')
+    register_password_field.setEchoMode(QLineEdit.Password)
+
+    register_password_repeat_field = QLineEdit()
+    register_password_repeat_field.setPlaceholderText('Repeat Password')
+    register_password_repeat_field.setEchoMode(QLineEdit.Password)
+
+    layout.addWidget(register_username_field)
+    layout.addWidget(register_password_field)
+    layout.addWidget(register_password_repeat_field)
+
+    window.setLayout(layout)
