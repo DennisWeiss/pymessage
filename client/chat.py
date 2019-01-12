86b78ebaf8223915dfee49b146f9a15c720f4f0f
Dennis Weiss <weiss.dennis98@yahoo.de>
2019-01-12 11:12:13 +0100

    Added friends are being stored in text file now

--- client/chat.py
+++ client/chat.py
@@ -10,15 +10,6 @@ socket_io = web_sockets.socket_io()
 with open('conf.json', encoding='utf-8') as f:
     conf = json.loads(f.read())
 
-session_obj = session.get_session()
-
-friends = []
-
-_user_id = None
-_auth_token = None
-
-warning_dialog = None
-
 
 def set_up_warning_dialog(window, message):
     layout = QVBoxLayout()
@@ -33,6 +24,20 @@ def set_up_warning_dialog(window, message):
     window.setLayout(layout)
 
 
+def add_friend(friend):
+    friends.append(friend)
+    file = open(conf['FRIENDS_FILE_NAME'], 'a')
+    file.write(friend + '\n')
+    file.close()
+
+
+def add_friend_to_overview(friend, overview):
+    friend_widget = QLabel()
+    friend_widget.setText(friend)
+    overview.addWidget(friend_widget)
+    friends_ui.append(friend_widget)
+
+
 def add_user(username, friends_overview):
     if username == _user_id:
         warning_dialog_window = QWidget()
@@ -45,10 +50,8 @@ def add_user(username, friends_overview):
         }
     )
     if response.status_code == 200:
-        friend = QLabel()
-        friend.setText(username)
-        friends_overview.addWidget(friend)
-        friends.append(friend)
+        add_friend(username)
+        add_friend_to_overview(username, friends_overview)
     elif response.status_code == 404:
         warning_dialog_window = QWidget()
         set_up_warning_dialog(warning_dialog_window, 'User ' + username + ' not found')
@@ -73,6 +76,9 @@ def setup_chat_window(window, user_id, auth_token):
 
     friends_overview = QVBoxLayout()
 
+    for friend in friends:
+        add_friend_to_overview(friend, friends_overview)
+
     add_user_field = QLineEdit()
     add_user_btn = QPushButton('Add User')
     add_user_btn.clicked.connect(lambda: add_user(add_user_field.text(), friends_overview))
@@ -87,4 +93,22 @@ def setup_chat_window(window, user_id, auth_token):
     window.setLayout(layout)
 
 
+def read_friends_from_file(file_name):
+    file = open(file_name, 'r')
+    friends = list(map(lambda string: string.rstrip(), file.readlines()))
+    file.close()
+    return friends
+
+
+session_obj = session.get_session()
+
+friends_file_name = conf['FRIENDS_FILE_NAME']
+
+friends = read_friends_from_file(friends_file_name)
+friends_ui = []
+
+_user_id = None
+_auth_token = None
+
+warning_dialog = None
 
