4775c88b740f285fe6a47a73729ee6644ecee341
Carl Schulze <carl.schulze@stud.hszg.de>
2019-01-16 14:17:45 +0100

    - register_window now closing after submit - Gui prints error message to User if the username is already taken or the password does not match

--- client/register.py
+++ client/register.py
@@ -1,4 +1,4 @@
-from PyQt5.QtWidgets import QLineEdit, QVBoxLayout, QPushButton
+from PyQt5.QtWidgets import QLineEdit, QVBoxLayout, QPushButton, QLabel
 import session
 import json
 
@@ -9,14 +9,18 @@ with open('conf.json', encoding='utf-8') as f:
 session_obj = session.get_session()
 
 
-def passwords_do_not_match():
-    # TODO: show info to user
-    print('Passwords do not match.')
+def passwords_do_not_match(info):
+
+    info.setText('Passwords do not match.')
+
+def user_already_exists(info):
+
+    info.setText('User already exists.')
 
 
-def register(username, password, password_repeat):
+def register(username, password, password_repeat, window, info):
     if password != password_repeat:
-        passwords_do_not_match()
+        passwords_do_not_match(info)
     else:
         response = session_obj.post(
             url=conf['SERVER_ADDRESS'] + '/register',
@@ -25,7 +29,12 @@ def register(username, password, password_repeat):
                 'password': password
             }
         )
-        print(response.text)
+
+        if "exists" in response.text:
+            user_already_exists(info)
+        else:
+            window.close()
+
 
 
 def setup_register_window(window):
@@ -44,11 +53,13 @@ def setup_register_window(window):
 
     register_btn = QPushButton('Register')
     register_btn.clicked.connect(lambda: register(register_username_field.text(), register_password_field.text(),\
-                                                  register_password_repeat_field.text()))
-
+                                                  register_password_repeat_field.text(),window,register_info))
+    register_info = QLabel()
     layout.addWidget(register_username_field)
     layout.addWidget(register_password_field)
     layout.addWidget(register_password_repeat_field)
+    layout.addWidget(register_info)
     layout.addWidget(register_btn)
+
 
     window.setLayout(layout)
